481c4fb225831892ff35b67e565c9ad6038c7fa7
Anurag Sharma <blstyle90@gmail.com>
2024-10-05 21:25:45 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+    fontFamily: {
+      leaguespartan: ["LeagueSpartan","sans-serif"],
+      opensans: ["OpenSans","sans-serif"],
+    },
+    colors: {
+      skin: "#fbf7ed",
+      black: "#212326",
+      mygreen: "#008060",
+      mydarkgreen: "#004C3F",
+      mylightgreen: "#F3FCF4",
+    },
+  },
+  },
+  plugins: [],
+}
+
